3e097130caa53fbede22495b50c67b1562b80893
koti-q <dshirnin9130@gmail.com>
2026-05-13 18:47:02 +0300

    Initial commit: Simple HTML page with vanilla js to test Green API

---
+++ src/script.js
@@ -0,0 +1,89 @@
+const apiUrl = "https://4100.api.green-api.com";
+
+function getCredentials() {
+  const idInstance = document.getElementById("idInstance").value;
+  const apiTokenInstance = document.getElementById("apiTokenInstance").value;
+
+  return { idInstance, apiTokenInstance };
+}
+
+function showResponse(data) {
+  document.getElementById("response").value =
+    JSON.stringify(data, null, 2);
+}
+
+async function getSettings() {
+  const { idInstance, apiTokenInstance } = getCredentials();
+
+  const response = await fetch(
+    `${apiUrl}/waInstance${idInstance}/getSettings/${apiTokenInstance}`
+  );
+
+  const data = await response.json();
+
+  showResponse(data);
+}
+
+async function getStateInstance() {
+  const { idInstance, apiTokenInstance } = getCredentials();
+
+  const response = await fetch(
+    `${apiUrl}/waInstance${idInstance}/getStateInstance/${apiTokenInstance}`
+  );
+
+  const data = await response.json();
+
+  showResponse(data);
+}
+
+async function sendMessage() {
+  const { idInstance, apiTokenInstance } = getCredentials();
+
+  const chatId = document.getElementById("chatIdMessage").value;
+  const message = document.getElementById("messageText").value;
+
+  const response = await fetch(
+    `${apiUrl}/waInstance${idInstance}/sendMessage/${apiTokenInstance}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chatId: chatId,
+        message: message,
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  showResponse(data);
+}
+
+async function sendFileByUrl() {
+  const { idInstance, apiTokenInstance } = getCredentials();
+
+  const chatId = document.getElementById("chatIdFile").value;
+  const urlFile = document.getElementById("fileUrl").value;
+
+  const response = await fetch(
+    `${apiUrl}/waInstance${idInstance}/sendFileByUrl/${apiTokenInstance}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+  body: JSON.stringify({
+    chatId: chatId,
+    urlFile: urlFile,
+    fileName: urlFile.split('/').pop().split('?')[0],
+  }),
+    }
+  );
+
+  const data = await response.json();
+
+  showResponse(data);
+}
